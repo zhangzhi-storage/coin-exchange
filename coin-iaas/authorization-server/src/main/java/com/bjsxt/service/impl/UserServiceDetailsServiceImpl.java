ebba4750c8c0a719c8064efe51757d5d9572d1f6
zhangzhi-storage <zhangzhi-storage>
2021-10-31 21:13:26 +0800

    代码提交

--- coin-iaas/authorization-server/src/main/java/com/bjsxt/service/impl/UserServiceDetailsServiceImpl.java
+++ coin-iaas/authorization-server/src/main/java/com/bjsxt/service/impl/UserServiceDetailsServiceImpl.java
@@ -170,9 +170,6 @@ public class UserServiceDetailsServiceImpl implements UserDetailsService {
     }
 
     public static void main(String[] args) {
-        BCryptPasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
-        String encode = passwordEncoder.encode("123456");
-//        System.out.println(encode);
         String hashpw = BCrypt.hashpw("123456", BCrypt.gensalt());
         System.out.println(hashpw);
 
