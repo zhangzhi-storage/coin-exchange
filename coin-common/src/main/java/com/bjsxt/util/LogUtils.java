82bc0313dfde67956d3d0b533566cbeb508a711e
zhangzhi-storage <zhangzhi-storage>
2021-11-09 22:15:56 +0800

    日志发送工具类

--- coin-common/src/main/java/com/bjsxt/util/LogUtils.java
+++ coin-common/src/main/java/com/bjsxt/util/LogUtils.java
@@ -2,7 +2,10 @@ package com.bjsxt.util;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -16,7 +19,21 @@ public class LogUtils {
         threadPool.execute(new Runnable() {
             @Override
             public void run() {
-                kafkaTemplate.send(topic,"dm",msg);
+                // 发送消息
+                ListenableFuture<SendResult<String, Object>> future = kafkaTemplate.send(kafkaTopic, jsonObj);
+                future.addCallback(new ListenableFutureCallback<SendResult<String, Object>>() {
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        log.info("Produce: The message failed to be sent:" + throwable.getMessage());
+                    }
+
+                    @Override
+                    public void onSuccess(SendResult<String, Object> stringObjectSendResult) {
+                        // TODO 业务处理
+                        log.info("Produce: The message was sent successfully:");
+                        log.info("Produce: _+_+_+_+_+_+_+ result: " + stringObjectSendResult.toString());
+                    }
+                });
             }
         });
     }
